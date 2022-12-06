0581e81efbc695888a9306abf5a57fb9ee6cc91b
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-12-06 20:18:19 +0100

    small update for production

--- src/components/appointments/AppointmentOverviewList.jsx
+++ src/components/appointments/AppointmentOverviewList.jsx
@@ -8,135 +8,166 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "moment-timezone";
 
-moment.tz.setDefault("Europe/Stockholm");
+// moment.tz.setDefault("Europe/Stockholm");
 
-const localizer = momentLocalizer(moment);
+// const localizer = momentLocalizer(moment);
 
-//substract 1 hour from the date to get the correct time if daylight saving time is not active
-function substractHourForDST(date) {
-  Date.prototype.stdTimezoneOffset = function () {
-    var jan = new Date(this.getFullYear(), 0, 1);
-    var jul = new Date(this.getFullYear(), 6, 1);
-    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
-  };
-  Date.prototype.isDstObserved = function () {
-    return this.getTimezoneOffset() < this.stdTimezoneOffset();
-  };
+// //substract 1 hour from the date to get the correct time if daylight saving time is not active
+// function substractHourForDST(date) {
+//   Date.prototype.stdTimezoneOffset = function () {
+//     var jan = new Date(this.getFullYear(), 0, 1);
+//     var jul = new Date(this.getFullYear(), 6, 1);
+//     return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+//   };
+//   Date.prototype.isDstObserved = function () {
+//     return this.getTimezoneOffset() < this.stdTimezoneOffset();
+//   };
 
-  if (date.isDstObserved()) {
-    return date;
-  } else {
-    date.setTime(date.getTime() - 1 * 60 * 60 * 1000);
-    return date;
-  }
-}
+//   if (date.isDstObserved()) {
+//     return date;
+//   } else {
+//     date.setTime(date.getTime() - 1 * 60 * 60 * 1000);
+//     return date;
+//   }
+// }
 
 export default memo(function AppointmentOverviewList() {
-  const { theme, oppositeTheme } = useThemeColors();
-  const [appointments, setAppointments] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const appointmentsApi = useAppointments();
-  const [events, setEvents] = useState([]);
+//   const { theme, oppositeTheme } = useThemeColors();
+//   const [appointments, setAppointments] = useState([]);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const appointmentsApi = useAppointments();
+//   const [events, setEvents] = useState([]);
 
-  const refreshAppointments = useCallback(async () => {
-    try {
-      setLoading(true);
-      setError(null);
-      const fetchedAppointments = await appointmentsApi.getAll();
-      setAppointments(fetchedAppointments);
-      const allEvents = fetchedAppointments.map((appointment) => {
-        const name =
-          `${appointment.user.firstName} ${appointment.user.lastName}` +
-          " : " +
-          `${appointment.training.name}`;
-        return {
-          id: appointment.id,
-          title: name,
-          start: substractHourForDST(new Date(appointment.startTime)),
-          end: substractHourForDST(new Date(appointment.endTime)),
-        };
-      });
-      setEvents(allEvents);
-      console.log(fetchedAppointments);
-      console.log(
-        `events: ${allEvents.forEach((event) =>
-          console.log(event.id, event.title, event.start, event.end)
-        )}`
-      );
-    } catch (error) {
-      console.error(error);
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+//   const refreshAppointments = useCallback(async () => {
+//     try {
+//       setLoading(true);
+//       setError(null);
+//       const fetchedAppointments = await appointmentsApi.getAll();
+//       setAppointments(fetchedAppointments);
+//       // const allEvents = fetchedAppointments.map((appointment) => {
+//       //   const name =
+//       //     `${appointment.user.firstName} ${appointment.user.lastName}` +
+//       //     " : " +
+//       //     `${appointment.training.name}`;
+//       //   return {
+//       //     id: appointment.id,
+//       //     title: name,
+//       //     start: substractHourForDST(new Date(appointment.startTime)),
+//       //     end: substractHourForDST(new Date(appointment.endTime)),
+//       //   };
+//       // });
+//       // setEvents(allEvents);
+//       // console.log(fetchedAppointments);
+//       // console.log(
+//       //   `events: ${allEvents.forEach((event) =>
+//       //     console.log(event.id, event.title, event.start, event.end)
+//       //   )}`
+//       // );
+//     } catch (error) {
+//       console.error(error);
+//       setError(error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }, []);
 
-  useEffect(() => {
-    refreshAppointments();
-  }, [refreshAppointments]);
+//   const refreshevents = useCallback(async () => {
+//     try {
+//       setLoading(true);
+//       setError(null);
 
-  const handleDelete = useCallback(async (idToDelete) => {
-    try {
-      setError(null);
-      await appointmentsApi.deleteById(idToDelete);
-      console.log("onDeleteConfirm", idToDelete);
-      setAppointments((appointments) =>
-        appointments.filter(({ id }) => id !== idToDelete)
-      );
-    } catch (error) {
-      console.error(error);
-      setError(error);
-    }
-  }, []);
+//       const allEvents = appointments.map((appointment) => {
+//         const name =
+//           `${appointment.user.firstName} ${appointment.user.lastName}` +
+//           " : " +
+//           `${appointment.training.name}`;
+//         return {
+//           id: appointment.id,
+//           title: name,
+//           start: substractHourForDST(new Date(appointment.startTime)),
+//           end: substractHourForDST(new Date(appointment.endTime)),
+//         };
+//       });
+//       setEvents(allEvents);
+//       console.log(
+//         `events: ${allEvents.forEach((event) =>
+//           console.log(event.id, event.title, event.start, event.end)
+//         )}`
+//       );
+//     } catch (error) {
+//       console.error(error);
+//       setError(error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }, [refreshAppointments]);
 
-  return (
-    <>
-      <div className={` mt-5 mx-5 pb-5`}>
-        <h1 className="py-2 text-center">Appointments</h1>
-        <Loader loading={loading} />
-        <Error error={error} />
-        {!loading && !error ? (
-          <Calendar
-            localizer={localizer}
-            min={substractHourForDST(new Date(0, 0, 0, 8, 0, 0))}
-            max={substractHourForDST(new Date(0, 0, 0, 19, 15, 0))}
-            defaultView="week"
-            startAccessor="start"
-            endAccessor="end"
-            events={events}
-            className={`calendar-${theme} btn-${theme}`}
-            style={{ height: "70vh", maxWidth: "1200px", margin: "auto" }}
-          />
-        ) : null}
-      </div>
-      <div className={`fullscreen bg-${theme} text-${oppositeTheme}`}>
-        {/* <h1 className="pt-5 text-center">Appointments</h1> */}
-        <div className="landscape">
-          <div className="d-flex flex-column mx-auto">
-            <h2 className="text-center">Appointment overview: </h2>
-            <br />
-            <h6 className="text-center"> Sorted by date:</h6>
-            <div className="apbox">
-              <Loader loading={loading} />
-              <Error error={error} />
-              {!loading && !error ? (
-                <>
-                  {appointments
-                    .sort((a, b) => new Date(a.date) - new Date(b.date))
-                    .map((appoint) => (
-                      <Appointment
-                        key={appoint.id}
-                        {...appoint}
-                        onDelete={handleDelete}
-                      />
-                    ))}
-                </>
-              ) : null}
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-});
+//   useEffect(() => {
+//     refreshAppointments();
+//   }, [refreshAppointments, refreshevents]);
+
+//   const handleDelete = useCallback(async (idToDelete) => {
+//     try {
+//       setError(null);
+//       await appointmentsApi.deleteById(idToDelete);
+//       console.log("onDeleteConfirm", idToDelete);
+//       setAppointments((appointments) =>
+//         appointments.filter(({ id }) => id !== idToDelete)
+//       );
+//     } catch (error) {
+//       console.error(error);
+//       setError(error);
+//     }
+//   }, []);
+
+//   return (
+//     <>
+//       <div className={` mt-5 mx-5 pb-5`}>
+//         <h1 className="py-2 text-center">Appointments</h1>
+//         <Loader loading={loading} />
+//         <Error error={error} />
+//         {!loading && !error ? (
+//           <Calendar
+//             localizer={localizer}
+//             min={substractHourForDST(new Date(0, 0, 0, 8, 0, 0))}
+//             max={substractHourForDST(new Date(0, 0, 0, 19, 15, 0))}
+//             defaultView="week"
+//             startAccessor="start"
+//             endAccessor="end"
+//             events={events}
+//             className={`calendar-${theme} btn-${theme}`}
+//             style={{ height: "70vh", maxWidth: "1200px", margin: "auto" }}
+//           />
+//         ) : null}
+//       </div>
+//       <div className={`fullscreen bg-${theme} text-${oppositeTheme}`}>
+//         {/* <h1 className="pt-5 text-center">Appointments</h1> */}
+//         <div className="landscape">
+//           <div className="d-flex flex-column mx-auto">
+//             <h2 className="text-center">Appointment overview: </h2>
+//             <br />
+//             <h6 className="text-center"> Sorted by date:</h6>
+//             <div className="apbox">
+//               <Loader loading={loading} />
+//               <Error error={error} />
+//               {!loading && !error ? (
+//                 <>
+//                   {appointments
+//                     .sort((a, b) => new Date(a.date) - new Date(b.date))
+//                     .map((appoint) => (
+//                       <Appointment
+//                         key={appoint.id}
+//                         {...appoint}
+//                         onDelete={handleDelete}
+//                       />
+//                     ))}
+//                 </>
+//               ) : null}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+ });
