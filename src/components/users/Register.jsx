165267fd5c8cb3fc3cb64f782a21a93e59deeddb
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-12-07 13:34:57 +0100

    update

--- src/components/users/Register.jsx
+++ src/components/users/Register.jsx
@@ -2,9 +2,18 @@ import { USERS } from "/src/api/mock-data.jsx";
 import { useState } from "react";
 import User from "/src/components/users/User.jsx";
 import RegistrationForm from "/src/components/users/RegistrationForm.jsx";
-import { updateToDateObject } from "/src/components/appointments/AppointmentList.jsx";
+// import { updateToDateObject } from "/src/components/appointments/AppointmentList.jsx";
 import { useThemeColors } from "/src/contexts/Theme.context.jsx";
 
+
+const updateToDateObject = (list, ...dateProps) => {
+  return list.map((entry) => {
+    for (const prop of dateProps) {
+      entry[prop] = new Date(entry[prop]);
+    }
+    return entry;
+  });
+};
 export default function Register() {
   const { theme, oppositeTheme } = useThemeColors();
   const [users, setUsers] = useState(USERS);
