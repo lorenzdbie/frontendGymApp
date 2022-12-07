cc1c068c3efd3336cb98b14b68b2afa7573fb30c
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-12-07 17:16:37 +0100

    test

--- src/components/afspraken/Afspraak.jsx
+++ src/components/afspraken/Afspraak.jsx
@@ -1,3 +1,22 @@
-export default function Afspraak(){
+import{memo, useCallback} from "react";
+import {Link} from 'react-router-dom';
+import {useThemeColors} from "/src/contexts/Theme.context.jsx";
+
+import {IoTrashOutline, IoPencilOutline} from "react-icons/io5";
+
+export default function Afspraak({id, user, date, training, startTime, endTime, intensity, specialRequest, onDelete}){
   console.log("test");
+
+  const {theme} = useThemeColors();
+
+  const durationInMinutes = (new Date(endTime).getTime() - new Date(startTime).getTime())/1000/60;
+
+  const handleDelete = useCallback(
+    (event) => {
+      event.preventDefault();
+      onDelete(id);
+    }
+  );
+
+  
 } 
