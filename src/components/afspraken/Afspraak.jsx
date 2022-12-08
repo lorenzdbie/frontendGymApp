d4922a80e5392fb71d0d6b5feb3fabb0e08a00f1
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-12-08 13:41:27 +0100

    aaanpassing layout

--- src/components/afspraken/Afspraak.jsx
+++ src/components/afspraken/Afspraak.jsx
@@ -1,8 +1,8 @@
-import{memo, useCallback} from "react";
-import {Link} from 'react-router-dom';
-import {useThemeColors} from "/src/contexts/Theme.context.jsx";
+import { memo, useCallback } from "react";
+import { Link } from "react-router-dom";
+import { useThemeColors } from "/src/contexts/Theme.context.jsx";
 
-import {IoTrashOutline, IoPencilOutline} from "react-icons/io5";
+import { IoTrashOutline, IoPencilOutline } from "react-icons/io5";
 
 function substractHourForDST(date) {
   Date.prototype.stdTimezoneOffset = function () {
@@ -22,101 +22,111 @@ function substractHourForDST(date) {
   }
 }
 
-export default function Afspraak({id, user, date, training, startTime, endTime, intensity, specialRequest, onDelete}){
+export default function Afspraak({
+  id,
+  user,
+  date,
+  training,
+  startTime,
+  endTime,
+  intensity,
+  specialRequest,
+  onDelete,
+}) {
   console.log("test");
 
-  const {theme} = useThemeColors();
+  const { theme } = useThemeColors();
 
-  const durationInMinutes = (new Date(endTime).getTime() - new Date(startTime).getTime())/1000/60;
+  const durationInMinutes =
+    (new Date(endTime).getTime() - new Date(startTime).getTime()) / 1000 / 60;
 
-  const handleDelete = useCallback(
-    (event) => {
-      event.preventDefault();
-      onDelete(id);
-    }
-  );
+  const handleDelete = useCallback((event) => {
+    event.preventDefault();
+    onDelete(id);
+  });
 
-  return(
+  return (
     <div className="appointment-box" data-cy="appointment">
-       <div className="appoitment-boxHead">
-         <div>
-           <span data-cy="appointment_id">Appointment # {id}</span>
-         </div>
-         <div className="btn-group float-end">
-           <Link
-             type="button"
-             className={`btn btn-${theme}`}
-             to={`/appointments/edit/${id}`}
-             data-cy="appointment_edit_btn"
-           >
-             <IoPencilOutline />
-           </Link>
-           <button
-             type="button"
-             className="btn btn-danger"
-             onClick={handleDelete}
-             data-cy="appointment_delete_btn"
-           >
-             <IoTrashOutline />
-           </button>
-         </div>
-       </div>
+      <div className="appoitment-boxHead">
+        <div>
+          <span data-cy="appointment_id">Appointment # {id}</span>
+        </div>
+        <div className="btn-group float-end">
+          <Link
+            type="button"
+            className={`btn btn-${theme}`}
+            to={`/appointments/edit/${id}`}
+            data-cy="appointment_edit_btn"
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
+            <IoPencilOutline />
+          </Link>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={handleDelete}
+            data-cy="appointment_delete_btn"
+          >
+            <IoTrashOutline />
+          </button>
+        </div>
+      </div>
 
-       <div>
-         <table>
+      <div>
+        <table>
           <tbody>
-             <tr>
-               <td>Trainee:</td>
-               <td data-cy="appointment_user">
-                 {user.firstName + " " + user.lastName}
-               </td>
-             </tr>
-             <tr>
-               <td>Date:</td>
-               <td data-cy="appointment_date">
-                 {new Date(date).toLocaleDateString("en-be", {
-                   weekday: "short",
-                   year: "numeric",
-                   month: "short",
-                   day: "numeric",
-                 })}
-               </td>
-             </tr>
-             <tr>
-               <td>Training:</td>
-               <td data-cy="appointment_training">{training.name}</td>
-             </tr>
-             <tr>
-               <td>Starts at:</td>
-               <td data-cy="appointment_startTime">
-                 {
-                 new Date(startTime).toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit", timeZone:"UTC" })}
-                
-               </td>
-             </tr>
-             <tr>
-               <td>Duration:</td>
-               <td data-cy="appointment_duration">
-                 {durationInMinutes}
-                 {" "}minutes
-               </td>
-             </tr>
-             <tr>
-               <td>intensity: </td>
-               <td data-cy="appointment_intensity">{intensity}</td>
-             </tr>
-             {specialRequest ? (
-               <tr>
-                 <td>Special request:</td>
+            <tr>
+              <td>Trainee:</td>
+              <td data-cy="appointment_user">
+                {user.firstName + " " + user.lastName}
+              </td>
+            </tr>
+            <tr>
+              <td>Date:</td>
+              <td data-cy="appointment_date">
+                {new Date(date).toLocaleDateString("en-be", {
+                  weekday: "short",
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </td>
+            </tr>
+            <tr>
+              <td>Training:</td>
+              <td data-cy="appointment_training">{training.name}</td>
+            </tr>
+            <tr>
+              <td>Starts at:</td>
+              <td data-cy="appointment_startTime">
+                {new Date(startTime).toLocaleTimeString("en-GB", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  timeZone: "UTC",
+                })}
+              </td>
+            </tr>
+            <tr>
+              <td>Duration:</td>
+              <td data-cy="appointment_duration">
+                {durationInMinutes} minutes
+              </td>
+            </tr>
+            <tr>
+              <td>intensity: </td>
+              <td data-cy="appointment_intensity">{intensity}</td>
+            </tr>
+            {specialRequest ? (
+              <tr>
+                <td>Special request:</td>
                 <td data-cy="appointment_specialRequest">{specialRequest}</td>
-               </tr>
-             ) : (
-               ""
-             )}
-           </tbody>
-         </table>
-       </div>
-     </div>
-   );
-  
-} 
+              </tr>
+            ) : (
+              ""
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
--- src/components/exercises/Exercise.jsx
+++ src/components/exercises/Exercise.jsx
@@ -1,5 +1,5 @@
 import { memo, useCallback } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useThemeColors } from "/src/contexts/Theme.context.jsx";
 
 import { IoTrashOutline, IoPencilOutline } from "react-icons/io5";
@@ -20,13 +20,14 @@ export default memo(function Exercise({ id, name, muscleGroup, onDelete }) {
   return (
     <div className="exercise-box" data-cy="exercise">
       <div className="exercise-boxHead">
-        <div data-cy="exercise_id" >{id}</div>
-        <div data-cy="exercise_name" >{name}</div>
+        <div data-cy="exercise_id">{id}</div>
+        <div data-cy="exercise_name">{name}</div>
         <div className="btn-group float-end">
           <Link
             type="button"
             className={`btn btn-${theme}`}
             to={`/exercises/edit/${id}`}
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
             data-cy="exercise_edit_btn"
           >
             <IoPencilOutline />
@@ -44,7 +45,6 @@ export default memo(function Exercise({ id, name, muscleGroup, onDelete }) {
 
       <div>
         <div>
-          
           <u> Active muscle groups:</u>
         </div>
         <div data-cy="exercise_muscleGroup"> {muscleGroup} </div>
