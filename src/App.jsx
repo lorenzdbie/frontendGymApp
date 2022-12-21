a80ec699e5e167aa1813364c9d10647e533cb4dc
Lorenz De Bie <lorenz.debie@student.hogent.be>
2022-12-21 18:27:02 +0100

    laatste puntjes op de i

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import RequireAuth from "/src/components/authentication/RequireAuth.jsx";
 function App() {
   const { theme, oppositeTheme } = useThemeColors();
 
+  
   return (
     <div className={`bg-${theme} text-${oppositeTheme}`}>
       <Navbar />
